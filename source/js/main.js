c185e27b5f4711387b2f2f060c78998f8b1d10ad
oltov <ururairurai@gmail.com>
2019-10-27 22:28:52 +0300

    исправил поведение навигации на десктопе

--- source/js/main.js
+++ source/js/main.js
@@ -4,7 +4,7 @@ const HEADER = document.querySelector('.header')
 const closeButton = HEADER.querySelector('.nav__button-close')
 const openButton = HEADER.querySelector('.button-menu')
 const NAV = HEADER.querySelector('.nav')
-const pageOff = document.querySelector('.page-on')
+const PAGE_OFF = document.querySelector('.page-on')
 const CLOSE_FORM = HEADER.querySelector('.form__button')
 const NAV_CALL = HEADER.querySelector('.button__any--nav')
 const FORM_HEADER = HEADER.querySelector('.form')
@@ -21,7 +21,7 @@ const REMOVE_ADD_CLASS = function (el, remove, add) {
 const ADD_LISTENER_FOR_LINKS = function () {
   LIST_LINKS.forEach(function (it) {
     it.addEventListener('click', function () {
-      REMOVE_ADD_CLASS(pageOff, 'page-off', 'page-on');
+      REMOVE_ADD_CLASS(PAGE_OFF, 'page-off', 'page-on');
       REMOVE_ADD_CLASS(NAV, 'nav--opened', 'nav--closed');
     })
   })
@@ -30,11 +30,11 @@ const ADD_LISTENER_FOR_LINKS = function () {
 const WORK_TAB_TABLET = function () {
   if (WIDTH_ELEM < WIDTH_DESKTOP) {
     openButton.addEventListener('click', function () {
-      REMOVE_ADD_CLASS(pageOff, 'page-on', 'page-off');
+      REMOVE_ADD_CLASS(PAGE_OFF, 'page-on', 'page-off');
       REMOVE_ADD_CLASS(NAV, 'nav--closed', 'nav--opened');
       ADD_LISTENER_FOR_LINKS();
       closeButton.addEventListener('click', function () {
-        REMOVE_ADD_CLASS(pageOff, 'page-off', 'page-on');
+        REMOVE_ADD_CLASS(PAGE_OFF, 'page-off', 'page-on');
         REMOVE_ADD_CLASS(NAV, 'nav--opened', 'nav--closed');
       })
       NAV_CALL.addEventListener('click', function () {
@@ -52,10 +52,12 @@ const WORK_TAB_TABLET = function () {
 const WORK_TAB_DESKTOP = function () {
   if (WIDTH_ELEM >= WIDTH_DESKTOP) {
     NAV_CALL.addEventListener('click', function () {
-      REMOVE_ADD_CLASS(pageOff, 'page-on', 'page-off');
+      REMOVE_ADD_CLASS(PAGE_OFF, 'page-on', 'page-off');
+      REMOVE_ADD_CLASS(NAV, 'nav--on', 'nav--off');
       REMOVE_ADD_CLASS(FORM_HEADER, 'form-off', 'form-on');
       CLOSE_FORM.addEventListener('click', function () {
-        REMOVE_ADD_CLASS(pageOff, 'page-off', 'page-on')
+        REMOVE_ADD_CLASS(PAGE_OFF, 'page-off', 'page-on')
+        REMOVE_ADD_CLASS(NAV, 'nav--off', 'nav--on');
         REMOVE_ADD_CLASS(FORM_HEADER, 'form-on', 'form-off');
       })
     })
