bdb95161997e40cda14cf92c70f885bd275d2474
oltov <ururairurai@gmail.com>
2019-10-28 15:00:54 +0300

    поправил js

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,62 +1,62 @@
 'use strict';
+(function () {
+  let multiItemSlider = function (selector) {
+    let mainElement = document.querySelector(selector)
+    let sliderWrapper = mainElement.querySelector('#slider__list')
+    let sliderItems = mainElement.querySelectorAll('.slider__item')
+    let sliderControls = mainElement.querySelectorAll('.slider__button')
+    // let sliderControlLeft = _mainElement.querySelector('.comment__link-left');
+    // let sliderControlRight = _mainElement.querySelector('.comment__link-right');
+    let wrapperWidth = parseFloat(getComputedStyle(sliderWrapper).width)
+    let itemWidth = parseFloat(getComputedStyle(sliderItems[0]).width)
+    let marginLeft = parseFloat(getComputedStyle(sliderItems[0]).marginLeft)
+    let marginRight = parseFloat(getComputedStyle(sliderItems[0]).marginRight)
+    let positionLeftItem = 0
+    let transform = 0
+    let step = itemWidth / wrapperWidth * 100 + ((marginLeft + marginRight) / wrapperWidth * 100)
+    let items = []
 
-let multiItemSlider = function (selector) {
-  let _mainElement = document.querySelector(selector)
-  let _sliderWrapper = _mainElement.querySelector('#slider__list')
-  let _sliderItems = _mainElement.querySelectorAll('.slider__item')
-  let _sliderControls = _mainElement.querySelectorAll('.slider__button')
-  // let _sliderControlLeft = _mainElement.querySelector('.comment__link-left');
-  // let _sliderControlRight = _mainElement.querySelector('.comment__link-right');
-  let _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width)
-  let _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width)
-  let _marginLeft = parseFloat(getComputedStyle(_sliderItems[0]).marginLeft)
-  let _marginRight = parseFloat(getComputedStyle(_sliderItems[0]).marginRight)
-  let _positionLeftItem = 0
-  let _transform = 0
-  let _step = _itemWidth / _wrapperWidth * 100 + ((_marginLeft + _marginRight) / _wrapperWidth * 100)
-  let _items = []
-  console.log((_marginLeft + _marginRight) / _wrapperWidth * 100)
-
-  _sliderItems.forEach(function (item, index) {
-    _items.push({ item: item, position: index, transform: 0 })
-  })
-
-  let position = {
-    getMin: 0,
-    getMax: _items.length - 1
-  }
-
-  let _transformItem = function (direction) {
-    if (direction === 'right') {
-      if (_positionLeftItem >= position.getMax) {
-        return
-      }
-      _positionLeftItem++
-      _transform -= _step
-    }
-    if (direction === 'left') {
-      if (_positionLeftItem <= position.getMin) {
-        return
-      }
-      _positionLeftItem--
-      _transform += _step
-    }
-    _sliderWrapper.style.transform = 'translateX(' + _transform + '%)'
-  }
-
-  let _controlClick = function (e) {
-    let direction = this.classList.contains('slider__link-right') ? 'right' : 'left'
-    _transformItem(direction)
-  }
-
-  let _setUpListeners = function () {
-    _sliderControls.forEach(function (item) {
-      item.addEventListener('click', _controlClick)
+    sliderItems.forEach(function (item, index) {
+      items.push({ item: item, position: index, transform: 0 })
     })
+
+    let position = {
+      getMin: 0,
+      getMax: items.length - 1
+    }
+
+    let transformItem = function (direction) {
+      if (direction === 'right') {
+        if (positionLeftItem >= position.getMax) {
+          return
+        }
+        positionLeftItem++
+        transform -= step
+      }
+      if (direction === 'left') {
+        if (positionLeftItem <= position.getMin) {
+          return
+        }
+        positionLeftItem--
+        transform += step
+      }
+      sliderWrapper.style.transform = 'translateX(' + transform + '%)'
+    }
+
+    let controlClick = function (e) {
+      let direction = this.classList.contains('slider__link-right') ? 'right' : 'left'
+      transformItem(direction)
+    }
+
+    let setUpListeners = function () {
+      sliderControls.forEach(function (item) {
+        item.addEventListener('click', controlClick)
+      })
+    }
+
+    setUpListeners()
   }
 
-  _setUpListeners()
-}
-
-multiItemSlider('.comment')
-multiItemSlider('.services')
+  multiItemSlider('.comment')
+  multiItemSlider('.services')
+})()
